637e743393b4fd48db3f55627279ce9205d26f50
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-25 09:33:45 -0700

    Initial Commit

---
+++ LearningSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningSwift
+//
+//  Created by Tului Gantulga on 5/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Tului Gantulga on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
