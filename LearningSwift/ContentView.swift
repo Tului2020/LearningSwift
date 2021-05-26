ccf866a1b8f690716c85460f09e2dd7d8f90a989
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-25 17:01:36 -0700

    learning about how buttons work

--- LearningSwift/ContentView.swift
+++ LearningSwift/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+
+            Button("Click Me") {
+                print("clicked")
+                
+            }
+        }
     }
 }
 
