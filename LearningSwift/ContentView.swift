2acbf6b6dcb1c09aea67fcce73cfe4adcd200bf0
Tului Gantulga <tgantulga@ucdavis.edu>
2021-06-11 17:20:40 -0700

    Fresh template

--- LearningSwift/ContentView.swift
+++ LearningSwift/ContentView.swift
@@ -9,82 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var playerCard = "card" + String(Int.random(in:2...14))
-    @State var cpuCard = "card" + String(Int.random(in:2...14))
-    @State var playerScore = 0
-    @State var cpuScore = 0
-    
     var body: some View {
         
-        
-        ZStack {
-            // Background (Color: Green)
-            Image("background")//.ignoresSafeArea()
-                .resizable()
-                .frame(height: 1000)
-                
-                
-
-            
-            VStack(spacing: 100) {
-                // 1. War Logo
-                Image("logo")
-                
-                
-                // 2. Cards
-                HStack(spacing: 100) {
-                    Image(playerCard)
-                    Image(cpuCard)
-                }
-                
-                // 3. Deal Button
-                
-                
-                Button(action: {
-                    
-                    let cpuCardNum = Int.random(in:2...14)
-                    let playerCardNum = Int.random(in:2...14)
-                    
-                    playerCard = "card" + String(playerCardNum)
-                    cpuCard = "card" + String(cpuCardNum)
-                    
-                    if (cpuCardNum > playerCardNum) {
-                        cpuScore += 1
-                    } else if (cpuCardNum < playerCardNum) {
-                        playerScore += 1
-                    }
-                    
-                    
-                    
-                    
-                    
-                }, label: {
-                    Image("dealbutton")
-                })
-                
-                
-                // 4. Game Stats
-                HStack(spacing: 10) {
-                    // 4a. Player and CPU
-                    VStack(spacing: 30) {
-                        Text("Player").foregroundColor(.white).font(.system(size: 20))
-                        Text(String(playerScore)).foregroundColor(.white).font(.system(size: 30))
-                    }
-                    
-                    // 4b. Score
-                    VStack(spacing: 30) {
-                        Text("CPU").foregroundColor(.white).font(.system(size: 20))
-                        Text(String(cpuScore)).foregroundColor(.white).font(.system(size: 30))
-                    }
-                }
-                
-                
-            }
-           
-                
-        }
-        .frame(height: 8.0)
-        
+        Text("Hello World")
         
     }
 }
